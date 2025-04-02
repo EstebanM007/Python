c08d4ffe2a23808786853378e6dee95ca76b14a4
EstebanM007 <bfwvTZw2P96pRhM>
2025-04-02 10:16:05 -0500

    Operaciones Matematicas

--- Python.py
+++ Python.py
@@ -1,1 +1,39 @@
+a = 1
+b = 2
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+h = a ** b
+i = a // b
+j = a & b
+k = a | b
+l = a ^ b
+m = a << b
+n = a >> b
+o = a == b
+p = a != b
+q = a > b
+r = a < b
+s = a >= b
+
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
+print(o)
+print(p)
+print(q)
+print(r)
+print(s)
+
 print("Hola Mundo")
